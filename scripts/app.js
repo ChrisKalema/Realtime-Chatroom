ef54c21e93fd83b67ad2b11b706fb612a5ef5c0d
unknown <chriskalma01@gmail.com>
2020-04-22 22:26:47 +0300

    added the functionality of adding a new chat (message) using an event listener

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,15 @@
 //DOM querries
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+//add a new chat (message)
+newChatForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(()=> newChatForm.reset())
+        .catch((error)=>console.log(error));
+});
 
 //class instances
 const chatUI = new ChatUI(chatList);
